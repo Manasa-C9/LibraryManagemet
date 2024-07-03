1a3f1846c0d22832fa0d08aada0e5503499340f5
Manasa-C9 <101011854+Manasa-C9@users.noreply.github.com>
2024-07-03 18:10:15 +0530

    User.java

--- User.java
+++ User.java
@@ -5,11 +5,27 @@ public class User {
 	String role;
 	String username;
 	String password;
+	private double balance;
 	
 	public User(String username,String password,String role)
 	{
 		this.username=username;
 		this.password=password;
 		this.role=role;
+		this.balance=0.0;
+	}
+	
+	void addBalance(double amount)
+	{
+		this.balance+=amount;
+	}
+	
+	void deductBalance(double amount)
+	{
+		this.balance-=amount;
+	}
+	
+	double getBalance() {
+		return this.balance;
 	}
 }
