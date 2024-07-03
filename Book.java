beea90e9574b9b1cb4eb60e0ea87c56cbe78d487
Manasa-C9 <101011854+Manasa-C9@users.noreply.github.com>
2024-07-03 18:09:30 +0530

    Book.java

--- Book.java
+++ Book.java
@@ -1,15 +1,39 @@
 package com.planonsoftware;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Book {
 	
 	String title;
 	String author;
 	int copiesAvailable;
-	public Book(String title,String author,int copiesAvailable)
+	double price;
+	LocalDate borrowDate;
+	boolean isRented;
+	static final int RENT_PERIOD_DAYS=14;
+	static final double DAILY_LATE_FEE=2.0;
+	
+	public Book(String title,String author,int copiesAvailable,double price)
 	{
 	  this.title=title;
 	  this.author=author;
 	  this.copiesAvailable=copiesAvailable;
+	  this.price=price;
+	  this.isRented=false;
 
 }
+	public double returnBook()
+	{
+		if(isRented)
+		{
+			isRented=false;
+			LocalDate returnDate=LocalDate.now();
+			Period period=Period.between(borrowDate, returnDate);
+			long daysRented=period.getDays();
+			long lateDays=daysRented-RENT_PERIOD_DAYS;
+			return lateDays>0?lateDays*DAILY_LATE_FEE:0.0;
+		}
+		return 0.0;
+	}
 }
